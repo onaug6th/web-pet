b8878950d6795a33a8cc13055eba143cd7875ec0
onaug6th <592986151@qq.com>
2019-01-02 18:52:59 +0800

    增加模板

--- src/web-pet/web-pet.ts
+++ src/web-pet/web-pet.ts
@@ -1,18 +1,37 @@
+import * as $ from "jquery"
 import { containerTpl } from "./template";
 
 /**
- * 组件渲染数据模型
+ * webPet配置模型
  */
 interface WebPetOptionsModel {
+    //  名称
     name?: string
-    status?: {
+    //  不可以做
+    cant?: {
+        //  走
+        move: boolean
+        //  自言自语
+        sayMyself: boolean
+        //  说笑
+        joke: boolean
+    }
+    //  状态图片
+    statusImg?: {
+        //  默认
         default: string
+        //  触摸
         hover: string
+        //  移动
         move: string
+        //  拖动
         drop: string
     }
+    //  服务端地址
     serverUrl?: {
+        //  学习
         learn: string
+        //  回答
         answer: string
     }
     [propName: string]: any
@@ -22,10 +41,13 @@ class WebPet {
     private options: WebPetOptionsModel = {};
 
     constructor() {
+        if(!window) throw new Error("抱歉，我只能在浏览器中花里胡哨");
+
         this.init();
     }
 
     private init() {
+        $(window.document.body).append(containerTpl);
         console.info("hello world, i am your web pet")
     }
 
