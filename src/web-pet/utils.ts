d28533d61d5e55668f740c1492197c5a2848f192
onaug6th <592986151@qq.com>
2019-01-25 09:59:06 +0800

    计算象限方法放到工具中管理

--- src/web-pet/utils.ts
+++ src/web-pet/utils.ts
@@ -20,7 +20,92 @@ const getJquery: Function = function () {
     });
 }
 
+/**
+ * 计算位置象限，待优化
+ * @param orgin 
+ * @param target 
+ */
+const countQuadrant: Function = function (orgin, target) {
+    const x: number = orgin.left;
+    const y: number = orgin.top;
+    const tx: number = target.left;
+    const ty: number = target.top;
+
+    //  1, 4
+    if (tx > x) {
+        //  1
+        if (ty > y) {
+            return 1;
+        }
+        //  4
+        if (ty < y) {
+            return 4;
+        }
+    }
+    //  2, 3
+    if (tx < x) {
+        //  2
+        if (ty < y) {
+            return 2;
+        }
+        //  3
+        if (ty > y) {
+            return 3;
+        }
+    }
+    if (ty == y) {
+        //  1, 2
+        if (ty > 0) {
+            //  1
+            if (tx > x) {
+                return 1;
+            }
+            //  2
+            if (tx < x) {
+                return 2;
+            }
+        }
+        //  3, 4
+        if (ty < 0) {
+            //  4
+            if (tx > x) {
+                return 4;
+            }
+            //  3
+            if (tx < x) {
+                return 3;
+            }
+        }
+    }
+    if (tx == x) {
+        //  1, 4
+        if (tx > 0) {
+            //  1
+            if (ty > y) {
+                return 1;
+            }
+            //  4
+            if (ty < y) {
+                return 4;
+            }
+        }
+        //  2, 3
+        if (tx < 0) {
+            //  2
+            if (ty > y) {
+                return 2;
+            }
+            //  3
+            if (ty < y) {
+                return 3;
+            }
+        }
+    }
+    return 4;
+}
+
 export {
     isFn,
-    getJquery
+    getJquery,
+    countQuadrant
 }
